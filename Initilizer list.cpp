473545fd71f5f357c50f3b5d04e8597093893397
MahmmadhusenDodiya <rajudodiya072@gmail.com>
2022-08-26 00:05:22 +0530

    initilizer List added

---
+++ Initilizer list.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+class A{
+public:
+	int a,b;
+	const int rate;  // we can't write rate=x here inside class
+	
+	int &z;  // 
+	A(int x,int y,int &r):a(x),rate(y),z(r)   // initializer list
+	{
+		z++;
+	}
+	
+};
+
+
+
+int main()
+{
+	int x=12;
+	A obj(1,2,x);
+	cout<<"x="<<x<<"\n";
+	return 0;
+}
