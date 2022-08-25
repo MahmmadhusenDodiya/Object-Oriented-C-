d7d4b91f5285b181db93fa4bbdf200986a114880
MahmmadhusenDodiya <rajudodiya072@gmail.com>
2022-08-25 23:45:39 +0530

    Abstract class added

---
+++ Abstract class.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// if we made function virtual then late binding happens
+
+
+// this is abstract class it contain pure virtual function
+// in Java there is abstract keyword but in c++ there is no
+class Person{
+	public:
+		virtual void fun()=0; 
+		// this is pure virtual function
+		
+	 
+};
+
+
+class Student:public Person
+{
+	public:
+		void fun()
+		{
+			
+		}
+};
+
+int main()
+{
+	
+	Student p;
+	p.fun();
+	
+	return 0;
+}
